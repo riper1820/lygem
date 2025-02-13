0ed03a4059abc4c1245c1eae3e49ca08009a2b99
riper1820 <94839457+riper1820@users.noreply.github.com>
2025-02-13 15:27:20 +0000

    fix build args

--- packages/engine/src/lib/buildPage.ts
+++ packages/engine/src/lib/buildPage.ts
@@ -1,3 +1,6 @@
+import { dirname, resolve } from "node:path"
+import { cwd } from "node:process"
+import { fileURLToPath } from "node:url"
 import { build } from "astro"
 
 export type BuildPageConfig = {
@@ -9,7 +12,11 @@ export const buildPage = async (config?: BuildPageConfig) => {
   if (config?.pageConfigPath) {
     process.env.LYGEM_CONFIG_PATH = config?.pageConfigPath
   }
+  const distPath = dirname(fileURLToPath(import.meta.url))
+  const packagePath = resolve(distPath, "..")
+  const outDir = resolve(cwd(), config?.exportPath ?? "dist")
   await build({
-    outDir: config?.exportPath,
+    root: packagePath,
+    outDir: outDir,
   })
 }
